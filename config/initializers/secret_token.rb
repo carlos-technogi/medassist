13fcf17b52cde0b68dd831c7866041fe4bc3c8ad
Carlos Hernandez <moguelator@yahoo.com>
2013-04-18 16:58:20 -0500

    Creación de Proyecto Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medreklk::Application.config.secret_key_base = '93cbe0a439e91c65b7dc42e854c3389644f94713df1b630eb5e7d64a9629ecfb4260a8ef0e91779e79bda8b837d7674c4a472bb0787c997d11a8f4e6082ae1f3'
